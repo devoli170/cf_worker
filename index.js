4b6f7fff3364c0cd146fec95e6ca958695c430d0
devoli170 <oliluib@gmail.com>
2026-03-02 14:37:20 +0100

    fixing src location

--- index.js
+++
@@ -1,8 +0,0 @@
-export default {
-  async fetch(request, env, ctx) {
-    const url = new URL(request.url);
-    // Forward the request to Google's API servers instead
-    url.hostname = 'generativelanguage.googleapis.com';
-    return fetch(new Request(url.toString(), request));
-  },
-};
